e923a8a528c16166691866ce12711ac657163d20
Sinisa Becic <sinisa.becic@outlook.com>
2024-03-20 23:11:15 +0100

    remove

--- src/services/axiosPlugin.ts
+++
@@ -1,8 +0,0 @@
-import { apiCallMovie } from "../services/api";
-import { App } from "vue";
-
-export default {
-  install: (app: App) => {
-    app.config.globalProperties.$http = apiCallMovie;
-  },
-};
