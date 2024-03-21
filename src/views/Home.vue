8f7d4722dad82a25a9f65ac0de301568e0842fcd
Sinisa Becic <sinisa.becic@outlook.com>
2024-03-21 01:54:49 +0100

    refactor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,10 @@
 <script lang="ts">
-import Navbar from "../components/header/Navbar.vue";
-import Searchbar from "../components/header/Searchbar.vue";
 import PopularMovies from "../components/movies/PopularMovies.vue";
 
 export default {
   name: "Home",
   components: {
     PopularMovies,
-    Navbar,
-    Searchbar,
   },
 };
 </script>
